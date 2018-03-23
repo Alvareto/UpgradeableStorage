b821c645c210b8e6841b7030ed17c7f46b2f1624
Ivan Grgurina <ivan.grgurina@gmail.com>
2018-03-23 18:58:11 +0100

    game update

--- metadata.lua
+++ metadata.lua
@@ -1,13 +1,13 @@
 return PlaceObj('ModDef', {
-	'title', "Upgradeable Waste Rock Dump Site",
-	'tags', "",
-	'id', "ranB3gH",
-	'author', "Alvareto",
-	'version', 30,
-	'lua_revision', 227923,
-	'code', {
-		"Code/PostBuildingUpgradeScript.lua",
-		"Code/customWasteRockDumpSmall.lua",
-	},
-	'saved', 1521814604,
+    'title', "Upgradeable Waste Rock Dump Site", 
+    'tags', "", 
+    'id', "ranB3gH", 
+    'author', "Alvareto", 
+    'version', 30, 
+    'lua_revision', 228184, 
+    'code', {
+        "Code/PostBuildingUpgradeScript.lua", 
+        "Code/customWasteRockDumpSmall.lua", 
+    }, 
+    'saved', 1521814604, 
 })
